713d245a985a05fa5b4c89b135affc32387836de
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2026-07-24 15:53:01 -0700

    Import

---
+++ build.sh
@@ -0,0 +1,10 @@
+mkapk.py \
+    -n AllowMsgs \
+    -v 0.0.1 \
+    -d src \
+    -a noarch \
+    -D 'Allow iMessage, WhatsApp and Signal text messages from the LAN' \
+    -u 'https://github.com/kn6plv/allowmsgs' \
+    -l 'MIT' \
+    -m 'tim.j.wilkinson@gmail.com' \
+    -o .
